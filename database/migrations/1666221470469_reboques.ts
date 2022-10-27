160524648b8a90ecf3ec9d4e34a585fa65939e14
_digsales <diogodobu@gmail.com>
2022-10-27 11:33:04 -0300

    Update Reboques migration #1

--- database/migrations/1666221470469_reboques.ts
+++ database/migrations/1666221470469_reboques.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id");
-      table.string("tipo_reboque");
+      table.string("tipo_reboque", 100).notNullable();
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
