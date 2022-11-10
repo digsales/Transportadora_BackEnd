a6fc812f8c4771a090adc12d96c4fd7e3d24755c
digsales <diogodobu@gmail.com>
2022-11-09 21:07:26 -0300

    Add Adonisjs Auth

--- start/routes.ts
+++ start/routes.ts
@@ -4,8 +4,13 @@ Route.get("/", async () => {
   return { servidor: "online" };
 });
 
-Route.resource("/caminhao", "CaminhaosController").apiOnly();
-Route.resource("/carga", "CargasController").apiOnly();
-Route.resource("/cliente", "ClientesController").apiOnly();
-Route.resource("/motorista", "MotoristasController").apiOnly();
-Route.resource("/reboque", "ReboquesController").apiOnly();
+Route.post("/store", "UsersController.store");
+Route.post("/login", "UsersController.login");
+
+Route.group(() => {
+  Route.resource("/caminhao", "CaminhaosController").apiOnly();
+  Route.resource("/carga", "CargasController").apiOnly();
+  Route.resource("/cliente", "ClientesController").apiOnly();
+  Route.resource("/motorista", "MotoristasController").apiOnly();
+  Route.resource("/reboque", "ReboquesController").apiOnly();
+}).middleware("auth");
